ff05d002d11166956956f30cd7e02a1059b94c46
Patrick Ratigan <patrickratigan@Patricks-MacBook-Pro.local>
2017-02-28 17:22:33 -0500

    first commit

---
+++ test.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.movie(
+	"title", "genre","release date","imbd url")
+	VALUES ('Toy Story','Animated Comedy', '22 November 1995', 'http://www.imdb.com/title/tt0114709/?ref_=nv_sr_1'), 
+    ('Jaws', 'Adventure', '20 June 1995', 'http://www.imdb.com/title/tt0073195/?ref_=fn_al_tt_1' ),
+    ('The Shawshank Redemption', 'Drama', '14 October 1994', 'http://www.imdb.com/title/tt0111161/?ref_=nv_sr_1'),
+    ('Ernest goes to camp', 'Comedy','22 May 1987','http://www.imdb.com/title/tt0092974/?ref_=nv_sr_1')
+    
+
+  
